44c723c0d33fe07ef7b7fa65e275089d5283bb71
Vlad <valdg2005@gmail.com>
2026-01-01 21:07:05 +0200

    Am sters fisierul inutil

--- EntitateCosmica.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Vlad on 17.11.2025.
-//
-
-#include "EntitateCosmica.h"
-
-//puteam pornide la 0 dar pare mai realist asa
-int EntitateCosmica::idGenerator = 337182;
--- ObiectCelest.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Vlad on 17.11.2025.
-//
-
-#include "ObiectCelest.h"
